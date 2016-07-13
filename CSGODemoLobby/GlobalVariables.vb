87924c15ee0599ad1dd7091322ba59315cc18c59
myluki2000 <lukas636@live.com>
2016-07-13 13:06:43 +0200

    "Setup" Design

---
+++ CSGODemoLobby/GlobalVariables.vb
@@ -0,0 +1,9 @@
+﻿Module GlobalVariables
+    Public Sub Sleep(time As Integer)
+        While time > 0
+            Threading.Thread.Sleep(10)
+            time -= 10
+            Application.DoEvents()
+        End While
+    End Sub
+End Module
--- CSGODemoLobby/My Project/Application.Designer.vb
+++ CSGODemoLobby/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CSGODemoLobby.Form1
+            Me.MainForm = Global.CSGODemoLobby.MainForm
         End Sub
     End Class
 End Namespace
